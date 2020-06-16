a546666083e07ba586de364ec933cc13d839ef8b
Mohammad Hammad Khan <66447399+MHKDESIGNS@users.noreply.github.com>
2020-06-16 04:26:58 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ import './Room.css';
 
 function App() {
   const [isLit, setLit] =useState(true);
+  let [age,setAge]=useState(10);
   // const brightness=isLit ? "lit" : "dark";
   return (
-    <div className={"room " +(isLit ? "lit" : "dark")}>      
+    <div className={`room ${isLit ? "lit" : "dark"}`}>      
           The room is {isLit ? "Light" : "Dark"}
           <br/>
           {/* <button onClick = {()=>console.log("button pressed")}>Toggle light</button> */}
           <button onClick = {()=>{
                                   setLit(!isLit)
                                    console.log("button pressed");}}>Toggle light</button>
+          <br/>
+          <button onClick = {()=>{
+                                  setAge(++age);
+                                  console.log("Age is increased" );}}> Increase age {age} </button>
+            
           
     </div>
   );
